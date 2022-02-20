2ecbd1a9d3cda25e272c1f21406bc8ae93615b94
john alejandro <john.e.alejandro01@gmail.com>
2022-02-20 00:04:25 -0600

    Completed data info fetch

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ const {url, type, id} = apiData
 const apiUrl = `${url}${type}/${id}`
 
 fetch(apiUrl)
-    .then( (data) =>  (data.json())
-    .then( (pokemon) => console.log(pokemon))
+    .then( (data) =>  data.json())
+    .then( (pokemon) => generateHtml(pokemon))
+
+const generateHtml = (data) => {
+    console.log(data)
+    const html = `
+    <div class="name">${data.name}</div>
+    <img src=${data.sprites.front_default}>
+    <div class="details">
+        <span>Height: ${data.height}</span>
+        <span>Weight: ${data.weight}</span>
+    </div>
+`
+    const pokemonDiv = document.querySelector('.pokemon')
+    pokemonDiv.innerHTML = html
+}
